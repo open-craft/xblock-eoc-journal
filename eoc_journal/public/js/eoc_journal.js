3d2614943bbec64aff66b7c0bdde1eb67841a8b2
Jillian Vogel <jill@opencraft.com>
2018-06-29 10:27:45 +0930

    Use new block name "Course Journal" in event and comments

--- eoc_journal/public/js/eoc_journal.js
+++ eoc_journal/public/js/eoc_journal.js
@@ -1,14 +1,14 @@
-/* Javascript loaded in the learner view of the EOC Journal */
+/* Javascript loaded in the learner view of the Course Journal */
 
 function EOCJournalXBlock(runtime, element) {
     // Attach Google Analytics tracking to a set of events
     var ga = window[window['GoogleAnalyticsObject'] || 'ga'];
     if (typeof ga == 'function') {
         $(element).find('a.pdf-report-link').click(function() {
-            ga('send', 'event', 'xblock-eoc-journal', 'click', 'PDF Report Download');
+            ga('send', 'event', 'Course Journal', 'click', 'PDF Report Download');
         });
         $(element).find('a.key-takeaways-link').click(function() {
-            ga('send', 'event', 'xblock-eoc-journal', 'click', 'Key Takeaways PDF Download');
+            ga('send', 'event', 'Course Journal', 'click', 'Key Takeaways PDF Download');
         });
     }
 }
