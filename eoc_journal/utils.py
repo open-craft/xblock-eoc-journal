41320d0ad28c6318cc69e39e88737acaf1d850d5
Alan Evangelista <alan@opencraft.com>
2020-02-26 20:47:02 -0300

    Fix tests broken by openedx import
    An import of Python package openedx was added in commit 4507e0d, but
    the test environment does not have that Python package. To avoid
    breaking the tests, this patch:
    
    - ignores the import error in Pylint check.
    - move the import to inside the function, which is not called in tests
      because the methods which call it are already mocked

--- eoc_journal/utils.py
+++ eoc_journal/utils.py
@@ -2,8 +2,6 @@
 
 from edx_rest_api_client.client import EdxRestApiClient
 
-from openedx.core.djangoapps.oauth_dispatch.jwt import create_jwt_for_user
-
 
 def normalize_id(key):
     """
@@ -29,6 +27,9 @@ def build_jwt_edx_client(url, scopes, user, expires_in, append_slash=True):
     Returns an edx API client authorized using JWT.
     """
 
+    # test env does not have edx-platform installed. because of this, all edx-platform imports must be
+    # kept inside this function so that the test code does not break when importing the module
+    from openedx.core.djangoapps.oauth_dispatch.jwt import create_jwt_for_user  # pylint: disable=import-error
     jwt = create_jwt_for_user(user)
     return EdxRestApiClient(url, append_slash=append_slash, jwt=jwt)
 
