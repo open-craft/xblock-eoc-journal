32c8ff6fa19ff0e9ef6f176cac6ee96049de3fa2
Shimul Chowdhury <shimul@opencraft.com>
2020-09-11 22:55:16 +0600

    manage.py should use workbench settings and problem builder

--- manage.py
+++ manage.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 import os
 import sys
+import workbench
+
+from django.conf import settings
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eoc_journal.settings")
-
+    xblock_sdk_dir = os.path.dirname(os.path.dirname(workbench.__file__))
+    sys.path.append(xblock_sdk_dir)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "workbench.settings")
+    settings.INSTALLED_APPS.append('problem_builder')
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
