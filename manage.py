d901e392945ce331f5035179000ebe48be927001
Shimul Chowdhury <shimul@opencraft.com>
2020-09-16 20:16:16 +0600

    revert manage.py

--- manage.py
+++ manage.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python
 import os
 import sys
-import workbench
-
-from django.conf import settings
 
 if __name__ == "__main__":
-    xblock_sdk_dir = os.path.dirname(os.path.dirname(workbench.__file__))
-    sys.path.append(xblock_sdk_dir)
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "workbench.settings")
-    settings.INSTALLED_APPS.append('problem_builder')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eoc_journal.settings")
+
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
